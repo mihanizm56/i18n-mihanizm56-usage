c1d5475417ba064754b11f3291829c7b9dda8517
mihanizm56 <m727507.56@mail.ru>
2020-05-04 03:45:55 +0300

    fix

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -4,11 +4,9 @@ import { rootReducer } from "./root-reducer";
 import { translationMiddleware } from "@mihanizm56/i18n-react";
 import { fetchLanguageKeysRequest } from "../../services/api/requests/fetch-language-keys";
 
-const LANG_URL = "http://127.0.0.1:5000/translations";
-
 export const createAppStore = () => {
   const composeMiddlewares = [
-    translationMiddleware({ url: LANG_URL, request: fetchLanguageKeysRequest }),
+    translationMiddleware(fetchLanguageKeysRequest),
   ];
 
   const enhancers = composeWithDevTools(applyMiddleware(...composeMiddlewares));
--- src/services/api/requests/fetch-language-keys/index.js
+++ src/services/api/requests/fetch-language-keys/index.js
@@ -1,2 +1,2 @@
-export const fetchLanguageKeysRequest = ({ lang, url }) =>
-  fetch(`${url}?lang=${lang}`).then((data) => data.json());
+export const fetchLanguageKeysRequest = (lang) =>
+  fetch(`http://127.0.0.1:8080/I18N/getTr?lang=${lang}`).then((data) => data.json());
